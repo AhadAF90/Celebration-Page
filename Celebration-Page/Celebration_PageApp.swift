fa688bca76396a0f06e64491b60d1c0a34edd816
Ahad <ahoudtiion@hotmail.com>
2023-10-16 16:51:46 +0300

    Initial Commit

---
+++ Celebration-Page/Celebration_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Celebration_PageApp.swift
+//  Celebration-Page
+//
+//  Created by Ahad on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Celebration_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
